303bdf8f00b9e4e0e9923e10bf91700b4c15d9c3
harpreen <nanda.harpreet@canada.ca>
2019-03-29 14:33:30 -0400

    Changes to remove the test end points

--- application.js
+++ application.js
@@ -1,27 +1,12 @@
 var express = require('express');
 var cors = require('cors');
-
 var app = express();
-
 // Enable CORS for all requests
 app.use(cors());
-
 // allow serving of static files from the public directory
 app.use(express.static(__dirname + '/public'));
-
-
 app.use('/apidocs', require('./lib/apidocs.js')());
-
 app.use('/userinfo', require('./lib/userinfo.js')());
-
-// test e.p
-app.use('/apidocstest', require('./lib/apidocsTest.js')());
-
-
-
-
-
-
 // default port is 8001
 var port = process.env.FH_PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var host = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
